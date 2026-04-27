807a2a39d7ece842cd59956181cdc5c26cfd8c5d
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-04-27 20:16:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from formulas import (calc_fccnogc, calc_rol, calc_fcgc, calc_fcid,
 
 st.title("Corporate Finance Calc")
 st.caption("v2.0")
-
+st.markdown("""
+---
+**License:** Apache 2.0  
+**Source code:** https://github.com/tuo-repo
+""")
 for key in ['fccnogc', 'rol', 'fcgc', 'fcid', 'fcfr', 'fcrf', 'fcu', 'pat_net_list', 'of_list']:
     if key not in st.session_state:
         st.session_state[key] = None
