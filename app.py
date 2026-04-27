a8435c0abb1b519a3418d9436a76a4135a223520
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-04-27 19:37:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -114,21 +114,20 @@ if page == "Cash Flow":
         acqui_2 = st.number_input("Acquisition 2", key="acqui_2", value=0.0)
 
         if st.button("Calculate FCID"):
-        res = calc_fcid(
-          fcid = fcid_a,
-          inv = inv,
-          dis = dis,
-          vnc = 0,
-          val_sto = val_sto,
-          plus = plus,
-          minus = minus,
-          ammo_ti = ammo_ti,
-          n_ammo = int(n_ammo),
-          acqui_1 = acqui_1,
-          acqui_2 = acqui_2
-        )
-        st.session_state.fcid = res
-        st.success(f"FCID = {res:.2f}")
+          res = calc_fcid(fcid = fcid_a,
+                          inv = inv,
+                          dis = dis,
+                          vnc = 0,
+                          val_sto = val_sto,
+                          plus = plus,
+                          minus = minus,
+                          ammo_ti = ammo_ti,
+                          n_ammo = int(n_ammo),
+                          acqui_1 = acqui_1,
+                          acqui_2 = acqui_2
+                         )
+          st.session_state.fcid = res
+          st.success(f"FCID = {res:.2f}")
  
     elif op == "FCFR":
         st.markdown("Insert here its components")
