cb9f2d04b9f7794fb3ae08b3b894f9e8ff5f0487
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-04-28 14:43:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ page = st.sidebar.radio("Select one", [
     "Stock Evaluation -coming soon",
     "Mortgage amortisation: French and Italian -coming soon",
     "NPV Comparision -coming soon",
-    "WACC",
+    "WACC -coming soon",
     "Portfolio -coming soon"
 ])
 
