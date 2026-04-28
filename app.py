fac74305c88fa3e13ed87044b8c4d27fcb147e1b
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-04-28 23:39:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -342,7 +342,8 @@ elif page == "NPV":
         else:
             st.info("NPV = 0: the project is neutral.")
           
-elif page == "Bond Evaluation":
+    
+    elif page == "Bond Evaluation -in progress":
     st.subheader("Zero Coupon Bond PV")
     st.markdown("Insert here its components")
   
