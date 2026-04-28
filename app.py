0092232f431eba1263c0d164ef214804729e68fe
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-04-28 02:22:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -305,7 +305,7 @@ elif page == "Bond Evaluation":
     st.markdown("Insert here its components")
   
     vn  = st.number_input("Face value (VN)", key="vn_zero", value=0.0)
-    k  = st.number_input("Discount rate k", key="k_zero", value=0.0, format="%.4f")
+    k = st.number_input("Discount rate k", key="k_zero", value=0.0, format="%.4f")
     dur = st.number_input("Duration (years)", key="dur_zero", value=0, step=1)
 
     if st.button("Calculate Zero Coupon Bond PV"):
