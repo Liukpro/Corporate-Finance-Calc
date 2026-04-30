9c558f7c416455adf770e8aea6351a40872905e4
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-04-30 20:01:19 +0200

    Create financial_schema.py

---
+++ financial_schema.py
@@ -0,0 +1,44 @@
+FINANCIAL_SCHEMA = {
+    "MOL": {
+        "canonical": "EBITDA",
+        "aliases": ["MOL", "EBITDA"]
+    },
+    "ROL": {
+        "canonical": "EBIT",
+        "aliases": ["ROL", "EBIT", "RO-L"]
+    },
+    "FCCNOGC": {
+        "canonical": "FCCNOGC",
+        "aliases": ["FCCNOGC"]
+    },
+    "FCGC": {
+        "canonical": "FCGC",
+        "aliases": ["FCGC"]
+    },
+    "FCID": {
+        "canonical": "FCID",
+        "aliases": ["FCID"]
+    },
+    "FCFR": {
+        "canonical": "FCFR",
+        "aliases": ["FCFR"]
+    },
+    "FCRF": {
+        "canonical": "FCRF",
+        "aliases": ["FCRF"]
+    },
+    "FCU": {
+        "canonical": "FCU",
+        "aliases": ["FCU"]
+    },
+    "FCE": {
+        "canonical": "FCE",
+        "aliases": ["FCE"]
+    },
+    "NPV": {
+        "canonical": "NPV",
+        "aliases": ["NPV"]
+    }
+}
+
+# To be further configured to add most of the aliases known: chiedere al professore
