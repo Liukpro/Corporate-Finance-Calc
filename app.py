832072ed19bcd868efe58ffba16efdb6fa1765b2
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-05-03 19:22:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -407,7 +407,7 @@ elif page == "NPV":
             k_values = np.arange(0,0.5, 0.01)
             npv_values = []
             for k_val in k_values:
-                npv_values.append(calc_npv(None, fc=fc_list, k=k_val, i_o = i_0, t = t_list, cost = cost))
+                npv_values.append(calc_npv(None, fc=fc_list, k=k_val, i_0 = i_0, t = t_list, cost = cost))
 
             chart_npv = pd.DataFrame({"k": k_values, "NPV": npv_values})
             st.line_chart(chart_npv, x = "k", y= "NPV")
