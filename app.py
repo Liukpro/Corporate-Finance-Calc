01f01d5ee2280fea4a42a26be03be9e2749c64c0
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-05-19 15:16:37 +0200

    Update app.py
    #updated the version

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from analisi_prospettica_area_caratteristica import (calc_mo_netto, calc_ammorta
 st.set_page_config(page_title="Corporate Finance Calc", layout="wide")
 
 st.title("Corporate Finance Calc")
-st.caption("v_alpha_3.1, #added metrics for Analisi Prospettica, # english-italian mix vocabs- to be updated")
+st.caption("v_alpha_3.2, #bugs in the mortgage section resolved, # english-italian mix vocabs- to be updated")
 
 st.sidebar.markdown("""
 **License:** Apache 2.0  
